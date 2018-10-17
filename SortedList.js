e2143c40e2600aa59f1a1398baf4fe6c2ad42b31
Spencer Brimley <Sjbrimley26@live.com>
2018-10-17 06:42:11 -0700

    Added some methods to the sortedlist

--- SortedList.js
+++ SortedList.js
@@ -1,15 +1,15 @@
-const Node = function(data) {
+const Node = function (data) {
   this.data = data;
   this.next = null;
 };
 
-const SortedList = function() {
+const SortedList = function () {
   this.head = null;
   this.tail = null;
   this.length = 0;
 };
 
-SortedList.prototype.push = function(data) {
+SortedList.prototype.push = function (data) {
   this.length++;
   const newNode = new Node(data);
 
@@ -36,7 +36,7 @@ SortedList.prototype.push = function(data) {
   }
 
   while (currentNode.data <= newNode.data) {
-    let nextNode = currentNode.next;
+    const nextNode = currentNode.next;
     if (nextNode.data >= newNode.data) {
       // console.log("Insert");
       newNode.next = nextNode;
@@ -50,7 +50,7 @@ SortedList.prototype.push = function(data) {
 SortedList.prototype.forEach = function (fn) {
   let currentVal = this.head;
   while (currentVal) {
-    fn(currentVal.data);
+    fn(currentVal);
     currentVal = currentVal.next;
   }
 };
@@ -62,7 +62,7 @@ SortedList.prototype.print = function () {
 SortedList.prototype.toArray = function () {
   const arr = [];
   this.forEach(arr.push.bind(arr));
-  return arr;
+  return arr.map(item => item.data);
 };
 
 SortedList.prototype.shift = function () {
@@ -70,18 +70,46 @@ SortedList.prototype.shift = function () {
   this.length--;
   const oldHead = this.head.data;
   this.head = this.head.next;
+  if (this.length === 0) {
+    this.tail = null;
+  }
   return oldHead;
 };
 
 SortedList.prototype.pop = function () {
+  if (!this.head) return undefined;
+  this.length--;
+  if (this.length === 0) {
+    const oldTail = this.head.data;
+    this.head = null;
+    this.tail = null;
+    return oldTail;
+  }
   const oldTail = this.tail.data;
   let newTail = this.head;
-  for (let i = 0; i < this.length - 2; i++) {
+  for (let i = 0; i < this.length - 1; i++) {
     newTail = newTail.next;
   }
   newTail.next = null;
   this.tail = newTail;
   return oldTail;
+};
+
+SortedList.prototype.reverse = function () {
+  if (!this.head) return undefined;
+  let prev = null;
+  let next = null;
+  let current = this.head;
+  while (current) {
+    next = current.next;
+    current.next = prev;
+    prev = current;
+    current = next;
+  }
+  const oldTail = this.tail;
+  this.tail = this.head;
+  this.head = oldTail;
+  return this;
 };
 
 const list = new SortedList();
@@ -96,8 +124,13 @@ sampleArray.forEach(i => {
   list.push(i);
 });
 
-console.log("Length", list.length);
-console.log(list.toArray());
+const log = () => console.log(list.toArray());
 
-// Sometimes not all of the items are added into the list.
-// especially if a 0 is entered after some other numbers
+console.log("Length", list.length);
+log();
+
+list.reverse();
+log();
+
+list.push(10).push(20);
+log();
